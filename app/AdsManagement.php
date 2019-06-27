994e0548d1342bbd55a47bea817175f451c23c98
Abdullah al mamun <abdullah.ssc13@gmail.com>
2019-06-27 18:01:47 +0600

    top add form and list design

---
+++ app/AdsManagement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdsManagement extends Model
+{
+    protected $connection = 'mysql';
+    protected $table = 'ads_management';
+}
